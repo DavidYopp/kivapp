5f3772e19c514ec16485bc3bcfd479920fe95de6
Josh Yopp <davidjyopp@gmail.com>
2020-06-03 13:51:09 -0400

    users can now cancel sign up and left right screen trasitions are set correctly

--- main.py
+++ main.py
@@ -8,7 +8,20 @@ Builder.load_file('design.kv')
 
 class LoginScreen(Screen):
     def sign_up(self):
+        self.manager.transition.direction = "left"
         self.manager.current = "sign_up_screen"
+
+
+    def login(self, uname, pword):
+        with open("users.json") as file:
+            users = json.load(file)
+            if uname in users and users[uname]["password"] == pword:
+                    self.ids.login_wrong.text = ""
+                    self.manager.transition.direction = "left"
+                    self.manager.current = "login_screen_success"
+            else:
+                self.ids.login_wrong.text = "User name or password incorrect, try again."
+
 
 class SignUpScreen(Screen):
     def add_user(self, uname, pword):
@@ -23,6 +36,24 @@ class SignUpScreen(Screen):
 
         with open("users.json", "w") as file:
             json.dump(users, file)
+
+        self.manager.current = "sign_up_screen_success"
+
+
+    def cancel_sign_up(self):
+        self.manager.transition.direction = "right"
+        self.manager.current = 'login_screen'
+
+
+class SignUpScreenSuccess(Screen):
+    def return_to_login(self):
+        self.manager.transition.direction = "right"
+        self.manager.current = "login_screen"
+
+class LoginScreenSuccess(Screen):
+    def log_out(self):
+        self.manager.transition.direction = "right"
+        self.manager.current = "login_screen"
 
 class RootWidget(ScreenManager):
     pass
