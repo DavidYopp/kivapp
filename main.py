22e312c64b79571a4ad42b5dc3c494e6a2387edc
Josh Yopp <davidjyopp@gmail.com>
2020-06-03 15:38:24 -0400

    added a hoverable logout button to the main screen after login as well as some styling

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.image import Image
+from kivy.uix.behaviors import ButtonBehavior
 from datetime import datetime
 from pathlib import Path
+from hoverable import HoverBehavior
 import random
 import json
 import glob
@@ -73,6 +76,9 @@ class LoginScreenSuccess(Screen):
             self.ids.quote.text = random.choice(quotes)
         else:
             self.ids.quote.text = "Try another feeling"
+
+class ImageButton(ButtonBehavior, HoverBehavior, Image):
+    pass
 
 class RootWidget(ScreenManager):
     pass
